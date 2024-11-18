b91c91a6e66f3748f285eb1cd34de0ef283d2210
sachinr0y <sachinroy9973@gmail.com>
2024-11-18 10:17:36 +0530

    Add your commit

---
+++ app/src/main/java/com/example/thenewsapp/repository/NewsRepository.kt
@@ -0,0 +1,18 @@
+package com.example.thenewsapp.repository
+
+import com.example.thenewsapp.api.RetrofitInstance
+import com.example.thenewsapp.db.ArticleDatabase
+import com.example.thenewsapp.models.Article
+
+class NewsRepository(val db: ArticleDatabase) {
+    suspend fun getHeadlines(countryCode: String, pageNumber:Int)=
+        RetrofitInstance.api.getHeadlines(countryCode,pageNumber)
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticle().upsert(article)
+
+    fun getFavouriteNews() = db.getArticle().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticle().deleteArticle(article)
+}
---
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/FavouritesFragment.kt
@@ -0,0 +1,125 @@
+package com.example.thenewsapp.ui.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thenewsapp.R
+import com.example.thenewsapp.adapters.NewsAdapter
+import com.example.thenewsapp.databinding.FragmentFavouritesBinding
+import com.example.thenewsapp.ui.NewsActivity
+import com.example.thenewsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
+
+
+class FavouritesFragment : Fragment(R.layout.fragment_favourites) {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var binding: FragmentFavouritesBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentFavouritesBinding.bind(view)
+
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setupFavouritesRecycler()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article",it)
+            }
+            findNavController().navigate(R.id.action_favouritesFragment_to_articleFragment, bundle)
+        }
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or  ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val article = newsAdapter.differ.currentList[position]
+                newsViewModel.deleteArticle(article)
+                Snackbar.make(view,"Removed from Favourites",Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo"){
+                        newsViewModel.addToFavourites(article)
+                    }
+                    show()
+                }
+            }
+        }
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.recyclerFavourites)
+        }
+
+        newsViewModel.getFavouriteNews().observe(viewLifecycleOwner, Observer { articles ->
+            newsAdapter.differ.submitList(articles)
+        })
+    }
+
+    private fun setupFavouritesRecycler(){
+        newsAdapter = NewsAdapter()
+        binding.recyclerFavourites.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+
+
+
+    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_favourites, container, false)
+//    }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment FavouritesFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            FavouritesFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
